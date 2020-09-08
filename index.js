690c3a59c6cba975b351265b44752d56546c98ae
jrluka13 <56405398+jrluka13@users.noreply.github.com>
2020-09-08 19:12:43 +0300

    singl ap with vue js

---
+++ index.js
@@ -0,0 +1,64 @@
+(async function loadData() {
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "test.html";
+}());
+let Ana_Sayfa = document.getElementById("Ana_Sayfa");
+Ana_Sayfa.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "test.html";
+  }
+let Kurulum = document.getElementById("Kurulum");
+  Kurulum.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "api.html";
+  }
+let Ürünler = document.getElementById("Ürünler");
+Ürünler.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "index2.html";
+  }
+  let Satın_Al = document.getElementById("Satın_Al");
+  Satın_Al.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "index3.html";
+  }
+  let Fatura_Design = document.getElementById("Fatura_Design");
+Fatura_Design.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "index4.html";
+  }
+  let ILTECH = document.getElementById("ILTECH");
+  ILTECH.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "iltechurunler.html";
+  }
+  let OPENCART = document.getElementById("OPENCART");
+  OPENCART.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "opencarturunler.html";
+  }
+  let N11 = document.getElementById("N11");
+  N11.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "urunler.html";
+  }
+//   let Gittigidiyor = document.getElementById("Gittigidiyor");
+//   Gittigidiyor.onclick = function(){
+//     var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+//     iframe.src = "test.html";
+//   }
+  let Hepsiburada = document.getElementById("Hepsiburada");
+  Hepsiburada.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "hburunler.html";
+  }
+  let Trendyol = document.getElementById("Trendyol");
+  Trendyol.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "trendyolurunler.html";
+  }
+  let My_profile = document.getElementById("My_profile");
+  My_profile.onclick = function(){
+    var iframe = document.getElementById("myiframe");  // One of the many ways to select your iframe
+    iframe.src = "information.html";
+  }
